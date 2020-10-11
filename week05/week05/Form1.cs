9d98111dbc65917879e88cc4911c03484609c8b1
Anna Eszter Seres <anna.seres2@stud.uni-corvinus.hu>
2020-10-11 17:45:06 +0200

    8

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -27,6 +27,25 @@ namespace week05
             Tick = context.Tick.ToList();
             dataGridView1.DataSource = Tick;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Tick select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
@@ -55,5 +74,8 @@ namespace week05
 
 
         }
+
+
+
     }
 }
