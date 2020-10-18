f1ffdf1f18672e088d5d3e7e5137a874bf4df46c
Anna Eszter Seres <anna.seres2@stud.uni-corvinus.hu>
2020-10-18 19:57:35 +0200

    5

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
 
@@ -23,6 +24,14 @@ namespace week06
         {
             InitializeComponent();
 
+            dataGridView1.DataSource = Rates;
+
+            Fuggveny();
+
+        }
+
+        public void Fuggveny()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -33,10 +42,29 @@ namespace week06
             };
 
             var response = mnbService.GetExchangeRates(request);
-
             var result = response.GetExchangeRatesResult;
 
-            dataGridView1.DataSource = Rates;
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
+
     }
 }
