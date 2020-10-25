4633d3ca61bc26428d5ed7d67cbf11e6fd3df633
Anna Eszter Seres <anna.seres2@stud.uni-corvinus.hu>
2020-10-25 15:04:15 +0100

    5

--- week7/week7/Form1.cs
+++ week7/week7/Form1.cs
@@ -13,7 +13,10 @@ using week7.Entities;
 namespace week7
 {
     public partial class Form1 : Form
+
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
