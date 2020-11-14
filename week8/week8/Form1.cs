d5a80c0dc74e81989d3034d2304159776cf04a50
Anna Eszter Seres <anna.seres2@stud.uni-corvinus.hu>
2020-11-14 14:53:38 +0100

    11

--- week8/week8/Form1.cs
+++ week8/week8/Form1.cs
@@ -16,14 +16,18 @@ namespace week8
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         public IToyFactory _factory;
 
         private IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value;
+                DisplayNext();
+            }
         }
-
+        
 
         public Form1()
         {
@@ -56,5 +60,28 @@ namespace week8
                 _toys.Remove(oldestToy);
             }
         }
+
+        
+
+        private void btnSelectCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnSelectBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+        }
+
     }
 }
